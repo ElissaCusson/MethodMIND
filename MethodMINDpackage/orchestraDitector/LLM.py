c0673beb8e302a44a3f35dcb0f2f9d4f1be23c81
Liam Watford Cendra <118737306+godricgugly@users.noreply.github.com>
2024-11-27 15:34:15 +0100

    git ignore update

--- MethodMINDpackage/orchestraDitector/LLM.py
+++ MethodMINDpackage/orchestraDitector/LLM.py
@@ -1,9 +1,9 @@
 import google.generativeai as genai
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 def llm_test():
-    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
     model = genai.GenerativeModel(model_name="gemini-1.5-flash")
     response = model.generate_content("Explain how AI works")
