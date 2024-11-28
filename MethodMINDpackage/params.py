1a9c7c15eec7cdf6a68fc416c16ae676830acb7f
Elissa Cusson <elissacusson@gmail.com>
2024-11-28 12:55:02 +0100

    added the embedding of the user input in the retrieval file

--- MethodMINDpackage/params.py
+++ MethodMINDpackage/params.py
@@ -5,4 +5,4 @@ PUBMED_API_KEY = os.environ.get("PUBMED_API_KEY")
 PUBMED_BASE_URL = os.environ.get("PUBMED_BASE_URL")
 PUBMED_SEARCH_STRATEGY = os.environ.get("PUBMED_SEARCH_STRATEGY")
 
-GEMINI_API_kEY = os.environ.get("GEMINI_API_kEY")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
