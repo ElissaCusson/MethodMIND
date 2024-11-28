eeb98178f61ec999a0af6a5379542b3e1780e23c
Jean-Marc ANDRE <jma@as3p.be>
2024-11-28 12:10:45 +0100

    First version of the database creation v2

--- MethodMINDpackage/params.py
+++ MethodMINDpackage/params.py
@@ -4,3 +4,7 @@ import os
 PUBMED_API_KEY = os.environ.get("PUBMED_API_KEY")
 PUBMED_BASE_URL = os.environ.get("PUBMED_BASE_URL")
 PUBMED_SEARCH_STRATEGY = os.environ.get("PUBMED_SEARCH_STRATEGY")
+
+# Database parameters
+DIRENV_DIR = os.environ.get("DIRENV_DIR")
+DATA_DIRECTORY = f"{DIRENV_DIR[1:]}/data" #the slice is for removing the first dash
